4c78903e643f6530f4eedd328a720198be9594b6
Ömer Faruk Koç <77229286+Fcyb3r@users.noreply.github.com>
2021-02-06 00:28:26 +0300

    Add project files.

---
+++ zarOyunu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace zarOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            zar zar1 = new zar(6);
+            Console.WriteLine(zar1.zarAt());
+            Console.WriteLine(zar1.zarAt());
+            Console.WriteLine(zar1.zarAt());
+            Console.WriteLine(zar1.zarAt());
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ zarOyunu/zar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zarOyunu
+{
+    class zar
+    {
+        ///zarın alabileceği mak deger
+        int makZarDeg;
+        Random random = new Random();
+public zar(int makZarDeg)
+        {
+            this.makZarDeg = makZarDeg;
+        }         
+        ///rasgele bir sayi için zar at
+        public int zarAt()
+        {
+            ///rasgele sayı üretecek
+            return random.Next(1,makZarDeg +1);
+        }
+
+
+
+
+
+    }
+}
